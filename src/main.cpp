c6a1f7c7d3b8fc5212ebb5d5b6453ca91b2f2056
Alexander Kaufmann <alexander.k347@outlook.de>
2022-04-27 16:07:16 +0200

    added files for uart-lib

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 
 extern "C" {
     #include "morse.h"
+    #include "uart.h"
 }
 
-#define LED PORTB5 // Bitte hier im Define ändern, da an mehreren Stellen die Pinnummer  genutzt wird.
+#define LED PORTB5 
 Morse MorseConfig;
 
 int main(void){
@@ -17,49 +18,16 @@ int main(void){
   Serial.begin(9600); //Zur Nutzung fürs Debugging
 
   DDRB |= (1 << LED); 
-
   MorseConfig=SetupMorse(LED);
 
   char string[] = "das ist setlsam"; //Das, was ausgegeben werden soll, hier ändern! Aber nur Kleinbuchstaben und Zahlen, keine Umlaute, kein ß.
   int breakcounter = 0;
-
-  for(int i = 0; i < 100; i++){ //Maximal 1000 Zeichen ausgeben
     
-    
-    Serial.println("Stelle:");
-    Serial.println(i); //Bestätigung dass Auswahlverfahren geglückt ist
-  
-
-    int stelle;
-
-    if (string[i] >= 97 && string[i] <= 122) {
-      stelle = string[i];
-    }/*
-    if (string[i] >= 90 && string[i] <= 65){
-      stelle = string[i];
-    }*/
-    else if (string[i] >= 48 && string[i] <= 57){
-      stelle = string[i];
-    }
-    else{
-      stelle = 0;
-      _delay_ms(200); //Pause zu nächstem Wort, blinken zur definitiven Anzeige
-      PORTB |= (1 << DDB5);
-      _delay_ms(1000);
-      PORTB &= ~(1 << DDB5);
-      breakcounter ++;
-      if (breakcounter >= 2){
-        break;
-      }
-    }
 
 
-    char letter = stelle;   
-    
-      MorseChar(&letter);
-      breakcounter = 0;
+
   
   
     //ACHTUNG: Breakcounter zurücksetzen!!!
-  }
+  
 }
