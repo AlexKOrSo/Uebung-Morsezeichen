200ff2ab7f3462d26e1ecfb2be08d4f5d430ad43
Alexander Kaufmann <alexander.k347@outlook.de>
2022-04-27 16:31:03 +0200

    added uart-functionality (just recieve, not tested yet)

--- lib/Uart/uart.h
+++ lib/Uart/uart.h
@@ -1,6 +1,22 @@
 #ifndef UART_H_
 #define UART_H_
 
+#include <stdint.h>
+#include "avr/io.h"
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+#ifndef UART_DELIM
+#define UART_DELIM ('\n')
+#endif
+
+#define BAUD_PRESCALE (((F_CPU / (baudrate * 16UL)))-1)
+
+void Uart_Init(uint32_t baudrate);
+
+uint8_t Uart_RecieveString(char* buffer,uint8_t length);
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,32 @@ extern "C" {
     #include "morse.h"
     #include "uart.h"
 }
-
+#define BAUD 9600
 #define LED PORTB5 
 Morse MorseConfig;
 
 int main(void){
 
-  Serial.begin(9600); //Zur Nutzung fürs Debugging
+
+  Uart_Init(BAUD);
+  uint8_t const text_length = 39;
+  uint8_t recievedChCts = 0;
+  char text[text_length+1]; 
 
   DDRB |= (1 << LED); 
   MorseConfig=SetupMorse(LED);
 
-  char string[] = "das ist setlsam"; //Das, was ausgegeben werden soll, hier ändern! Aber nur Kleinbuchstaben und Zahlen, keine Umlaute, kein ß.
-  int breakcounter = 0;
+  while (1)
+  {
+    recievedChCts = Uart_RecieveString(text,text_length);
+
+    for (uint8_t i = 0; i < recievedChCts; i++)
+    {
+      MorseChar(text+i);
+    }
     
+  }
+  
 
 
 
